7418b13535667dd426b36384d427fe8cbe062700
Joscelin <joscelinlee@gmail.com>
2015-12-07 18:40:03 +0800

    Convert GET /todos to sequelize

--- server.js
+++ server.js
@@ -16,27 +16,48 @@ app.get('/', function(req, res) {
 
 // GET /todos?completed=true&q=house --> return description that has 'house'
 app.get('/todos', function(req, res) {
-	var queryParams = req.query;
-	var filteredTodos = todos;
+	var query = req.query;
+	var where = {};
 
-	if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
-		filteredTodos = _.where(filteredTodos, {
-			completed: true
-		});
-	} else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
-		filteredTodos = _.where(filteredTodos, {
-			completed: false
-		});
+	if (query.hasOwnProperty('completed') && query.completed === 'true') {
+		where.completed = true;
+	} else if (query.hasOwnProperty('completed') && query.completed === 'false') {
+		where.completed = false;
 	}
 
-	if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
-		filteredTodos = _.filter(filteredTodos, function(todo) {
-			return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1; // return -1 if does not exist, else return position
-		});
+	if (query.hasOwnProperty('q') && query.q.length > 0) {
+		where.description = {
+			$like: '%' + query.q + '%'
+		};
 	}
-	//"Go to work on Saturday".indexOf('work'); // return -1 if does not exist, else return position
-	// res.json(todos); // todos array is converted to json and send back to whoever call the api
-	res.json(filteredTodos);
+
+	db.todo.findAll({where: where}).then(function(todos) {
+		res.json(todos);
+	}, function(e) {
+		res.status(500).send();
+	});
+
+	// var queryParams = req.query;
+	// var filteredTodos = todos;
+
+	// if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
+	// 	filteredTodos = _.where(filteredTodos, {
+	// 		completed: true
+	// 	});
+	// } else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
+	// 	filteredTodos = _.where(filteredTodos, {
+	// 		completed: false
+	// 	});
+	// }
+
+	// if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
+	// 	filteredTodos = _.filter(filteredTodos, function(todo) {
+	// 		return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1; // return -1 if does not exist, else return position
+	// 	});
+	// }
+	// //"Go to work on Saturday".indexOf('work'); // return -1 if does not exist, else return position
+	// // res.json(todos); // todos array is converted to json and send back to whoever call the api
+	// res.json(filteredTodos);
 });
 
 // GET /todos/:id --> /todos/2
